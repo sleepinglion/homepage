1142c820d861910728ad546838aae6368b47b886
잠자는사자(Mac) <admin@sleepinglion.pe.kr>
2023-11-14 16:42:05 +0900

    modified:   app/javascript/packs/application.js 	modified:   babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -54,10 +54,16 @@ module.exports = function(api) {
         }
       ],
       [
-        '@babel/plugin-transform-private-methods'
+        '@babel/plugin-transform-private-methods',
+        {
+          loose: true
+        }
       ],
       [
-        '@babel/plugin-transform-private-property-in-object'
+        '@babel/plugin-transform-private-property-in-object',
+        {
+          loose: true
+        }
       ],
       [
         '@babel/plugin-transform-runtime',
