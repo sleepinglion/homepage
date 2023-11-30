6dfcf7b3af140045215e9fe0756f8ae817d6b311
잠자는사자(Mac) <admin@sleepinglion.pe.kr>
2023-11-30 22:25:46 +0900

    modified:   app/models/blog.rb

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -9,8 +9,8 @@ class Blog < ApplicationRecord
   belongs_to :blog_category, :autosave => true, :counter_cache => true
   has_one :blog_content, :foreign_key => :id, :dependent => :destroy, inverse_of: :blog
   has_many :blog_comment, :dependent => :destroy
-  accepts_nested_attributes_for :blog_content, :allow_destroy => true
-  accepts_nested_attributes_for :blog_comment, :allow_destroy => true
+  accepts_nested_attributes_for :blog_content, :allow_destroy => true, :update_only => true
+  accepts_nested_attributes_for :blog_comment, :allow_destroy => true, :update_only => true
   translates :title, :description
   mount_uploader :photo, BlogUploader
 
