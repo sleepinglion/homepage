ad47e5875b33e6aa0c7a3473c3097bc8b5edddc9
sleepinglion <admin@sleepinglion.pe.kr>
2024-12-19 20:01:47 +0900

    modified:   Gemfile 	modified:   Gemfile.lock 	modified:   config/environments/production.rb 	new file:   config/initializers/session_store.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,10 @@ Rails.application.configure do
 
   config.active_record.cache_versioning = false
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :redis_cache_store, {
+    url: ENV.fetch("REDIS_CACHE_URL") { "redis://localhost:6379/0" },
+    namespace: "cache"
+  }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
