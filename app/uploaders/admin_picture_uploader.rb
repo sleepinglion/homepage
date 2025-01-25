1c86eb791d6d885e272cbb713c3b39630788fa2e
sleepinglion(desktop) <admin@sleepinglion.pe.kr>
2025-01-25 12:49:44 +0900

    수정

--- app/uploaders/admin_picture_uploader.rb
+++ app/uploaders/admin_picture_uploader.rb
@@ -1,7 +1,7 @@
 class AdminPictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  include CarrierWave::RMagick
+  #include CarrierWave::MiniMagick
 
   storage :file
 
