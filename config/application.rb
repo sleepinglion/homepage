0dd616a336fc0b3601c9f5a42086a2c491435c95
sleepinglion <admin@jedaeroweb.co.kr>
2026-01-11 16:15:54 +0900

    수정함:        config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -19,15 +19,14 @@ module Sleepinglion
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     #config.i18n.load_path += Dir[Rails.root.join('locales', '*.{rb,yml}').to_s]
 
-    config.i18n.available_locales = [:ko, :en, :'zh-CN']
+    config.i18n.available_locales = [:ko, :en,  :ja, :'zh-CN']
 
     config.i18n.default_locale = :ko
     config.i18n.fallbacks = [:ko]
-
     config.i18n.enforce_available_locales = true
 
     # Configuration for the application, engines, and railties goes here.
-    #
+    config.assets.enabled = true
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
